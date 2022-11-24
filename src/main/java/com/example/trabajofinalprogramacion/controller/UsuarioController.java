0fdd1d9888a086eafe3a1ce757487a11d55d4357
JDCC1004 <juliand.cruzc@uqvirtual.edu.co>
2022-11-23 21:45:09 -0500

    Torres de Hanoi Recursivas

--- src/main/java/com/example/trabajofinalprogramacion/controller/UsuarioController.java
+++ src/main/java/com/example/trabajofinalprogramacion/controller/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.example.trabajofinalprogramacion.controller;
 
+import com.example.trabajofinalprogramacion.logic.MyExceptions;
+import com.example.trabajofinalprogramacion.logic.Nombre;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,7 +20,7 @@ import java.util.Objects;
 
 public class UsuarioController {
 
-    private String nombre;
+    Nombre nombre = new Nombre();
 
     @FXML
     private Label aviso;
@@ -28,17 +30,25 @@ public class UsuarioController {
     private Button btnJugar;
 
     @FXML
-    void saveNombre(ActionEvent event){
-        nombre = tfNombre.getText();
-        btnJugar.requestFocus();
-        System.out.println("Bienvenido: " +nombre);
+    void saveNombre(ActionEvent event) {
+
+            try {
+                nombre.setNombre((tfNombre.getText()));
+                nombre.validarNumerosNombre(tfNombre.getText());
+            } catch (MyExceptions ex) {
+                aviso.setText(ex.getMessage());
+            }
+            btnJugar.requestFocus();
+            tfNombre.clear();
     }
 
     @FXML
-     void OnActionJugar(ActionEvent event) throws IOException {
-        if (tfNombre.getText().isEmpty()) {
-            aviso.setVisible(true);
-        } else {
+     void OnActionJugar(ActionEvent event) throws IOException{
+
+        if(tfNombre.getText().isBlank()){
+            aviso.setText("!! Debe ingresar un nombre ¡¡");
+            tfNombre.requestFocus();
+        }else {
 
             Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/example/trabajofinalprogramacion/imgs/Inicio.png")));
 
@@ -57,12 +67,11 @@ public class UsuarioController {
             //nombre = tfNombre.getText();
             //System.out.println("Bienvenido " +nombre);
 
-            juegoControl.llenarNombre(nombre);
+            juegoControl.llenarNombre(tfNombre.getText());
 
             Stage stage2 = (Stage) this.btnJugar.getScene().getWindow();
             stage2.close();
         }
-
     }
 
     @FXML
